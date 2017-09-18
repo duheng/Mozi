7a1374716a9c5afea57f10aff814b9972a22cfe6
shengyao <MY_NAME@example.com>
2017-09-18 15:45:51 +0800

    pre

--- src/containers/mo/rule/index.js
+++ src/containers/mo/rule/index.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react"
-import {StyleSheet, Text, View} from "react-native"
+import React, { Component } from "react"
+import { StyleSheet, Text, View } from "react-native"
 
 export default class Rule extends Component {
   static navigationOptions = {
