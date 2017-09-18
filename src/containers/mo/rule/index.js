292bc9b43aded75d0ba4fbdd2d359096f7b5f871
shengyao <MY_NAME@example.com>
2017-09-18 16:14:52 +0800

    update readme

--- src/containers/mo/rule/index.js
+++ src/containers/mo/rule/index.js
@@ -1,28 +1,29 @@
-import React, {Component} from "react"
-import {StyleSheet, Text, View} from "react-native"
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
+    textAlign: 'center',
     margin: 10,
   },
-})
+});
 export default class Rule extends Component {
   static navigationOptions = {
-    headerTitle: "墨规范",
-  }
+    headerTitle: '墨规范',
+  };
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>墨规范!</Text>
       </View>
-    )
+    );
   }
 }
