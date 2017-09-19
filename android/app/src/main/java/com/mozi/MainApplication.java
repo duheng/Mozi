1f39bcc01de381c8e9a3a3a62a0fcfeb458f623e
shengyao <MY_NAME@example.com>
2017-09-19 22:53:45 +0800

    添加启动屏幕ios部分

--- android/app/src/main/java/com/mozi/MainApplication.java
+++ android/app/src/main/java/com/mozi/MainApplication.java
@@ -3,6 +3,7 @@ package com.mozi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new VectorIconsPackage()
       );
     }
