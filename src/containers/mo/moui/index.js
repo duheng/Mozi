ca0aa7d5148ffc74961dc6802db90ecdc9b337d1
shengyao <MY_NAME@example.com>
2017-09-28 11:32:17 +0800

    添加跳转到指定页面demo

--- src/containers/mo/moui/index.js
+++ src/containers/mo/moui/index.js
@@ -1,29 +1,41 @@
-import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from "react"
+import { StyleSheet, Text, View } from "react-native"
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
 })
 
 export default class Moui extends Component {
   static navigationOptions = {
-    headerTitle: '墨组件',
+    headerTitle: "墨组件",
+  }
+
+  goTab = () => {
+    const { navigation } = this.props
+    navigation.navigate("Gong")
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>分享!</Text>
+        <Text
+          style={styles.welcome}
+          onPress={() => {
+            this.goTab()
+          }}
+        >
+          跳转到指定tab页面
+        </Text>
         <Text style={styles.welcome}>Toase!</Text>
         <Text style={styles.welcome}>Loading!</Text>
         <Text style={styles.welcome}>Tips!</Text>
