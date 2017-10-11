1103ab9c959c8ad9ce44354807f4e5af897c42ed
shengyao <MY_NAME@example.com>
2017-10-11 13:13:03 +0800

    pre-commit

--- src/app/constants/urls.js
+++ src/app/constants/urls.js
@@ -1,4 +1,7 @@
 // const HOST = 'https://box-test.wepiao.com/api'
 const HOST = 'https://piaofang.wepiao.com/api';
-export const PIAOFANG_BOX_WILLPLAY = `${HOST}/v2/lib/movie/soon`;
+const PIAOFANG_BOX_WILLPLAY = `${HOST}/v2/lib/movie/soon`;
 
+module.exports = {
+  PIAOFANG_BOX_WILLPLAY,
+};
