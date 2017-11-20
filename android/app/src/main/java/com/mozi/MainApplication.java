500e70bff55e3b835df450e9d2d98a623d95a5c8
duheng <duheng1100@163.com>
2017-11-20 18:27:28 +0800

    升级到0.50

--- android/app/src/main/java/com/mozi/MainApplication.java
+++ android/app/src/main/java/com/mozi/MainApplication.java
@@ -29,6 +29,11 @@ public class MainApplication extends Application implements ReactApplication {
             new VectorIconsPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
--- index.ios.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-
-import Mozi from './src/root';
-
-AppRegistry.registerComponent('Mozi', () => Mozi);
