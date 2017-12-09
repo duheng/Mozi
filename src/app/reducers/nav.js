68744489c5738c8ebc6082f6cbf7251da34a4e78
shengyao <MY_NAME@example.com>
2017-12-09 09:31:04 +0800

    修复路径错误bug

--- src/app/reducers/nav.js
+++ src/app/reducers/nav.js
@@ -1,18 +1,18 @@
-import Routers from 'routers/app';
+import Routers from "../../routers/app"
 
-const FlagRoutes = new Set(); // 防止短时间内多次点击多次跳转
+const FlagRoutes = new Set() // 防止短时间内多次点击多次跳转
 const nav = (state, action) => {
-  if (action.type === 'Navigation/NAVIGATE') {
+  if (action.type === "Navigation/NAVIGATE") {
     if (FlagRoutes.has(action.routeName)) {
-      return state;
+      return state
     }
-    FlagRoutes.add(action.routeName);
+    FlagRoutes.add(action.routeName)
     setTimeout(() => {
-      FlagRoutes.delete(action.routeName);
-    }, 500);
+      FlagRoutes.delete(action.routeName)
+    }, 500)
   }
-  const newState = Routers.router.getStateForAction(action, state);
-  return newState || state;
-};
+  const newState = Routers.router.getStateForAction(action, state)
+  return newState || state
+}
 
-export default nav;
+export default nav
